f46eedf46689dbba85a5133f4bd6dbb4b3c10067
Ullash0 <shaon7187@gmail.com>
2024-11-02 23:34:57 +0600

    Create owner.js

---
+++ scripts/cmds/owner.js
@@ -0,0 +1,64 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+config: {
+  name: "owner",
+  aurthor:"Tokodori",// Convert By Goatbot Tokodori 
+   role: 0,
+  shortDescription: " ",
+  longDescription: "",
+  category: "admin",
+  guide: "{pn}"
+},
+
+  onStart: async function ({ api, event }) {
+  try {
+    const ownerInfo = {
+      name: 'ULLASH',
+      gender: 'Male',
+      age: '21',
+      height: 'Unknown',
+      facebookLink: 'https://www.facebook.com/profile.php?id=100086680386976',
+      nick: 'ABRONIL- আমার প্রিয় মানুষটা ডুবে থাকুক 🙂 , হৃদত্তপিন্ডের বিশাল গভিরতায়❤️‍🩹      MISS YOU MAYABOTI'
+    };
+
+    const bold = 'https://i.imgur.com/49v4Gl8.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
+
+    const tmpFolderPath = path.join(__dirname, 'tmp');
+
+    if (!fs.existsSync(tmpFolderPath)) {
+      fs.mkdirSync(tmpFolderPath);
+    }
+
+    const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
+    const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
+
+    fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
+
+    const response = `
+Owner Information:🧾
+Name: ${ownerInfo.name}
+Gender: ${ownerInfo.gender}
+Age: ${ownerInfo.age}
+Height: ${ownerInfo.height}
+Facebook: ${ownerInfo.facebookLink}
+Nick: ${ownerInfo.nick}
+`;
+
+
+    await api.sendMessage({
+      body: response,
+      attachment: fs.createReadStream(videoPath)
+    }, event.threadID, event.messageID);
+
+    if (event.body.toLowerCase().includes('ownerinfo')) {
+      api.setMessageReaction('🚀', event.messageID, (err) => {}, true);
+    }
+  } catch (error) {
+    console.error('Error in ownerinfo command:', error);
+    return api.sendMessage('An error occurred while processing the command.', event.threadID);
+  }
+},
+};
